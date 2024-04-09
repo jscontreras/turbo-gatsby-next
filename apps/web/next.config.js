6faf9c2570bcacb9264c152d981ca361af453d68
Sebastian Contreras <js.contreras@outlook.com>
2024-04-09 13:50:23 -0500

    Redirects config.

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
   transpilePackages: ["@repo/ui"],
+  async rewrites() {
+    return [
+      {
+        source: '/recipes/:path*',
+        destination: 'https://gatsby.tc-vercel.dev/recipes/:path*',
+      },
+    ]
+  },
 };
