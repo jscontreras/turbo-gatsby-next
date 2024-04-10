897c7d86a09e5eb80a73499ba8450925342f97f0
Sebastian Contreras <js.contreras@outlook.com>
2024-04-10 10:02:35 -0500

    Testing vercel function.

---
+++ apps/gatsby-demo/api/[...slug].js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const path = require('path');
+
+const fileExtensions = ['.js', '.json'];
+
+export function GET(_request) {
+    const files = listFilesSync();
+    return new Response(JSON.stringify(files), {
+      headers: {
+        'Content-Type': 'application/json',
+        'Vercel-CDN-Cache-Control':'max-age=3600',
+        'CDN-Cache-Control':'max-age=60',
+        'Cache-Control':'max - age=10',
+      },
+      status: 200 // HTTP status code for OK
+    });
+}
+
+/**
+ * Print Files from public folder
+ * @param {*} dirPath
+ * @param {*} arrayOfFiles
+ * @returns
+ */
+function getAllFilesSync(dirPath, arrayOfFiles = []) {
+  const files = fs.readdirSync(dirPath, { withFileTypes: true });
+
+  files.forEach(file => {
+    if (file.isDirectory()) {
+      arrayOfFiles = getAllFilesSync(path.join(dirPath, file.name), arrayOfFiles);
+    } else {
+      if (fileExtensions.some(ext => {
+        return file.name.endsWith(ext);
+      })) {
+        const fPath = path.join(dirPath, file.name).split('/public').pop();
+        arrayOfFiles.push(fPath);
+      }
+    }
+  });
+
+  return arrayOfFiles;
+}
+
+// Usage example
+function listFilesSync() {
+  try {
+    const dirPath = path.join(__dirname, '../public'); // Replace with your directory path
+    const files = getAllFilesSync(dirPath);
+    return files;
+  } catch (error) {
+    console.error('Error reading directory:', error);
+    return [];
+  }
+}
+
